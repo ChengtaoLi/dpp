97894190cc92ed61b95bd561485cd3fea0960a45
ChengtaoLi <ctli.cs@hotmail.com>
2017-12-28 17:25:30 +0800

    update

--- demo_classification.py
+++ demo_classification.py
@@ -25,31 +25,35 @@ k_group = [10,20,30,50,70,100]
 error_unif = np.zeros(len(k_group))
 error_dpp = np.zeros(len(k_group))
 
-for k_idx in xrange(len(k_group)):
-	k = k_group[k_idx]
-	# Uniform sampling
-	unif_smpl = np.random.permutation(nTrn)[:k]
+for run_id in xrange(5):
+	for k_idx in xrange(len(k_group)):
+		k = k_group[k_idx]
+		# Uniform sampling
+		unif_smpl = np.random.permutation(nTrn)[:k]
 
-	C = L[np.ix_(range(nTrn+nTst), unif_smpl)]
-	W = C[np.ix_(unif_smpl, range(k))]
-	X_prime = C.dot(inv(np.real(sqrtm(W))))
-	trnX_prime = X_prime[:nTrn]
-	tstX_prime = X_prime[nTrn:]
-	
-	error_unif[k_idx] = lr.train_predict(trnX_prime, trnY, tstX_prime, tstY)
+		C = L[np.ix_(range(nTrn+nTst), unif_smpl)]
+		W = C[np.ix_(unif_smpl, range(k))]
+		X_prime = C.dot(inv(np.real(sqrtm(W))))
+		trnX_prime = X_prime[:nTrn]
+		tstX_prime = X_prime[nTrn:]
+		
+		error_unif[k_idx] += lr.train_predict(trnX_prime, trnY, tstX_prime, tstY)
 
-	# DPP
-	D, V = utils.get_eig(trnL, flag_gpu=flag_gpu)
-	E = utils.get_sympoly(D, k, flag_gpu=flag_gpu)
-	dpp_smpl  = dpp.sample(D, V, E=E, k=k, flag_gpu=flag_gpu)
+		# DPP
+		D, V = utils.get_eig(trnL, flag_gpu=flag_gpu)
+		E = utils.get_sympoly(D, k, flag_gpu=flag_gpu)
+		dpp_smpl  = dpp.sample(D, V, E=E, k=k, flag_gpu=flag_gpu)
 
-	C = L[np.ix_(range(nTrn+nTst), dpp_smpl)]
-	W = C[np.ix_(dpp_smpl, range(k))]
-	X_prime = C.dot(inv(np.real(sqrtm(W))))
-	trnX_prime = X_prime[:nTrn]
-	tstX_prime = X_prime[nTrn:]
+		C = L[np.ix_(range(nTrn+nTst), dpp_smpl)]
+		W = C[np.ix_(dpp_smpl, range(k))]
+		X_prime = C.dot(inv(np.real(sqrtm(W))))
+		trnX_prime = X_prime[:nTrn]
+		tstX_prime = X_prime[nTrn:]
 
-	error_dpp[k_idx] = lr.train_predict(trnX_prime, trnY, tstX_prime, tstY)
+		error_dpp[k_idx] += lr.train_predict(trnX_prime, trnY, tstX_prime, tstY)
+
+error_unif /= 5.
+error_dpp /= 5.
 
 plt.figure(figsize=(4,4))
 plt.title('Approximate Kernel LR Test Error')
