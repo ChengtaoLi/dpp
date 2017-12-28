695e2ff5df5174dc57d3767078195933ac03b9eb
ChengtaoLi <ctli.cs@hotmail.com>
2017-12-28 15:42:52 +0800

    init

---
+++ helper/lr.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+import torch
+from torch.autograd import Variable
+from torch import optim
+
+from data_util import load_mnist
+
+
+def build_model(input_dim, output_dim):
+    # We don't need the softmax layer here since CrossEntropyLoss already
+    # uses it internally.
+    model = torch.nn.Sequential()
+    model.add_module("linear",
+                     torch.nn.Linear(input_dim, output_dim, bias=False))
+    return model
+
+
+def train(model, loss, optimizer, x_val, y_val):
+    x = Variable(x_val, requires_grad=False)
+    y = Variable(y_val, requires_grad=False)
+
+    # Reset gradient
+    optimizer.zero_grad()
+
+    # Forward
+    fx = model.forward(x)
+    output = loss.forward(fx, y)
+
+    # Backward
+    output.backward()
+
+    # Update parameters
+    optimizer.step()
+
+    return output.data[0]
+
+
+def predict(model, x_val):
+    x = Variable(x_val, requires_grad=False)
+    output = model.forward(x)
+    return output.data.numpy().argmax(axis=1)
+
+
+def train_predict(trX, trY, teX, teY):
+    torch.manual_seed(42)
+
+    trX = torch.from_numpy(trX).float()
+    teX = torch.from_numpy(teX).float()
+    trY = torch.from_numpy(trY).long()
+
+    n_examples, n_features = trX.size()
+    n_classes = 10
+    model = build_model(n_features, n_classes)
+    loss = torch.nn.CrossEntropyLoss(size_average=True)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    batch_size = 100
+
+    for i in range(100):
+        cost = 0.
+        num_batches = n_examples // batch_size
+        for k in range(num_batches):
+            start, end = k * batch_size, (k + 1) * batch_size
+            cost += train(model, loss, optimizer,
+                          trX[start:end], trY[start:end])
+        predY = predict(model, teX)
+        print("Epoch %d, cost = %f, acc = %.2f%%"
+              % (i + 1, cost / num_batches, 100. * np.mean(predY == teY)))
+
+    return 100. * (1 - np.mean(predY == teY))
+
